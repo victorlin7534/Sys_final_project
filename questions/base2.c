3a64ea17f177cd3bf42daa3a692b703f83d839cf
LarWong <lwong7@stuy.edu>
2019-01-11 11:19:49 -0500

    q

---
+++ questions/base2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Given a number in base 10, return the same number in base 2*/
+
+int base2(int a){
+
+
+
+}
---
+++ questions/gcf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Given two numbers, find the greatest common factor*/
+
+int gcd(int a, int b){
+
+
+
+
+
+}
